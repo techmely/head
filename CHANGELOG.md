4944b56e37f1593fff84ce66ddd31714fbffb5bf
harrytran998 <nhattq.coding@gmail.com>
2024-07-23 16:04:33 +0700

    fix build version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @techmely/head
 
+## 0.2.0
+
+### Minor Changes
+
+- add compose head metadata
+
 ## 0.1.1
 
 ### Patch Changes
--- build.ts
+++ build.ts
@@ -1,13 +1,17 @@
 import { execSync } from "node:child_process";
+import json from "./package.json";
 
 execSync("rm -rf dist");
 await Bun.build({
   entrypoints: ["src/index.ts"],
   outdir: "dist",
   target: "bun",
+  format: "esm",
+  splitting: true,
   sourcemap: "inline",
 });
 
 execSync("bun run tsc --emitDeclarationOnly --outDir dist");
 execSync("mv dist/src/* dist");
+execSync("rm -rf dist/src");
 execSync("rm -rf dist/tsconfig.tsbuildinfo");
